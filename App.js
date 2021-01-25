6e952448ab3da28b5aa4bbd8c545ec26da140574
Elizabeth Eidelson <fluturecode@gmail.com>
2021-01-24 23:26:52 -0500

    added buttons to App.js

--- App.js
+++ App.js
@@ -1,6 +1,32 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect, useState } from "react"
+import * as ImagePicker from "expo-image-picker"
+import Screen from "./app/components/Screen"
+import { Button, Image } from "react-native"
 
 export default function App() {
-  return <ListingEditScreen />;
+	const [imageUri, setImageUri] = useState()
+	const requestPermission = async () => {
+		const { granted } = await ImagePicker.requestCameraRollPermissionsAsync()
+		if (!granted) alert("You need to enable permissions to access.")
+	}
+
+	useEffect(() => {
+		requestPermission()
+	}, [])
+
+	const selectImage = async () => {
+		try {
+			const result = await ImagePicker.launchImageLibraryAsync()
+			if (!result.cancelled) setImageUri(result.uri)
+		} catch (error) {
+			console.log("Error reading image", error)
+		}
+	}
+
+	return (
+		<Screen>
+			<Button title='Select Image' onPress={selectImage} />
+			<Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+		</Screen>
+	)
 }
